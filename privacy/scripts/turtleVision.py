df96eb926e8e0c9d66389cb7cebdb5d71ec10449
Levi Scott <lscott15@cornellcollege.edu>
2014-06-27 12:18:12 -0500

    Added a turtleVision launch file and changed turtleVision to draw polygons rather than rectangles

--- privacy/scripts/turtleVision.py
+++ privacy/scripts/turtleVision.py
@@ -3,7 +3,9 @@
 import rospy
 from sensor_msgs.msg import Image, CameraInfo
 from image_geometry import PinholeCameraModel
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PolygonStamped
+from geometry_msgs.msg import Point32
 import csv 
 import cv, cv2
 import numpy
@@ -13,65 +15,99 @@ import tf
 from rosCV import rosCV as rcv
 
 class turtleViz():
-	def __init__(self, topic):
+	def __init__(self, topic, frames, polygons):
+		# The frames and shapes we want to blur
+		self.frameLocations = frames
+		self.polygonLocations = polygons
+
+		# To make the projection to a pixel
 		self.camModel = PinholeCameraModel()
+
+		# To look up the transforms between our points and the camera
 		self.listener = tf.TransformListener()
+	
+		# Alex's image stuff?
 		self.rcv = rcv()
+
+		# Our program is driven by this callback
 		rospy.Subscriber(topic, Image, self.image_callback)
 
+		# To get which frame is the camera frame
 		rospy.Subscriber("/camera/rgb/camera_info", CameraInfo, self.camera_callback)
-		self.haveCamera = False
+		
+		# A topic for the edited image data
 		self.pub = rospy.Publisher('turtleVision', Image)
 
 	def camera_callback(self, cameraInfo):
 		self.cameraInfo = cameraInfo
 		self.camModel.fromCameraInfo(cameraInfo)
-		# if not self.haveCamera:
-		# #we update our camModel with the cameraInfo to work some magic.
-		#     self.camModel.fromCameraInfo(cameraInfo)
-		#     self.haveCamera = True
 
 	def image_callback(self, image_in):
-		frameLocations = "/nfs/attic/smartw/users/reume02/catkin_ws/src/privacy-interfaces/privacy/config/frames/newThing.csv"
-		with open(frameLocations, 'rb') as csvfile:
+		image_cv2 = self.rcv.toCv2(image_in)
+		framesList = []
+		with open(self.frameLocations, 'rb') as csvfile:
 			reader = csv.reader(csvfile, delimiter=',', quotechar="'")
 			for row in reader:
-				newMarker = PoseStamped()
-				newMarker.header.frame_id = row[1]
-				newMarker.pose.position.x = float(row[2])
-				newMarker.pose.position.y = float(row[3])
-				newMarker.pose.position.z = float(row[4])
-			
-				newMarker.pose.orientation.x = 0.0
-				newMarker.pose.orientation.y = 0.0
-				newMarker.pose.orientation.z = 0.0
-				newMarker.pose.orientation.w = 0.0
-		# Import and convert
-		image_cv2 = self.rcv.toCv2(image_in)
-		try:
-			# newPose = self.listener.transformPose(self.cameraInfo.header.frame_id, newMarker)
-			newPose = self.listener.transformPose('/cam_pos', newMarker)
-			position = (newPose.pose.position.x, newPose.pose.position.y, newPose.pose.position.z)
-			projected = self.camModel.project3dToPixel(position)
-			rospy.logdebug(projected)
-			x = int(projected[0])
-			y = int(projected[1])
+				# Make a list of the frames
+				framesList.append(row[0])
 
-			image_cv2 = self.rcv.redact(image_cv2, (x,y), (x+50,y+50))
-			# Convert back to ROS Image msg
-			image_out = self.rcv.toRos(image_cv2)
-			self.pub.publish(image_out)
-		# except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException, AttributeError):
-		except ():
-			rospy.logdebug("transform failed")
-			# pass
+		for frame in framesList:
+			offset = self.polygonLocations + frame + ".csv"
+			polyPoints = []
+
+			# For visualization of polygons in rviz
+			polygon = PolygonStamped()
+			polygonPub = rospy.Publisher('polygons/' + frame, PolygonStamped)
+
+			# Polygons in rviz are relative to camera frame
+			polygon.header.frame_id = "camera_rgb_optical_frame"
+			with open(offset, 'rb') as csvfile:
+				reader = csv.reader(csvfile, delimiter=',', quotechar="'")
+				
+				for row in reader:
+					newPolyPoint = PointStamped()
+					newPolyPoint.header.frame_id = frame
+					newPolyPoint.point.x = float(row[0])
+					newPolyPoint.point.y = float(row[1])
+					newPolyPoint.point.z = float(row[2])
+					# We don't care about orientation
+					
+					try:
+						#Transform the point to our published privacy frame
+						transformedPolyPoint = self.listener.transformPoint(self.cameraInfo.header.frame_id, newPolyPoint)
+
+						# If the point is behind the camera, don't project it or add it to the polygon
+						if not transformedPolyPoint.point.z <= 0:
+							# Get the pixel from that transformed point and append it to the list of points
+							positionPoint = (transformedPolyPoint.point.x, transformedPolyPoint.point.y, transformedPolyPoint.point.z)
+							projectedPoint = self.camModel.project3dToPixel(positionPoint)
+							polyPoints.append(projectedPoint)
+
+						# For rviz
+						polygon.polygon.points.append(Point32(transformedPolyPoint.point.x, transformedPolyPoint.point.y, transformedPolyPoint.point.z))
+					except():
+						pass
+				pointArray = numpy.asarray(polyPoints, numpy.int32)
+				# Only draw the shape if it has more than 2 points
+				if len(pointArray) > 2:
+					image_cv2 = self.rcv.redactPolygon(image_cv2, pointArray)
+		
+			# For rviz
+			polygonPub.publish(polygon)
+
+		# Convert back to ROS Image msg
+		image_out = self.rcv.toRos(image_cv2)
+		self.pub.publish(image_out)
 		self.rcv.imshow(image_cv2)
 
 
 if __name__ == '__main__':
 
 	rospy.init_node('turtleVision', log_level=rospy.DEBUG)
+	frames = rospy.get_param('turtleVision/frameLocations', "/home/local/CORNELL-COLLEGE/lscott15/privacy/src/privacy-interfaces/privacy/config/frames/cornell.csv")
+	polygons = rospy.get_param('turtleVision/polygonLocations', "/home/local/CORNELL-COLLEGE/lscott15/privacy/src/privacy-interfaces/privacy/config/polygons/cornell/")
+	topic = rospy.get_param('turtleVision/image_topic', "/camera/rgb/image_color_repub")
 
-	tv = turtleViz('/camera/rgb/image_color')
+	tv = turtleViz(topic, frames, polygons)
 
 	rospy.spin()
