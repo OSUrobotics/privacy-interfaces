5f8d4fa930da01f7b5a96e94e4748a0ce2cab61b
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-06-27 18:26:12 -0700

    Octrees! Before, it saved octrees, but now it is broken.

---
+++ octomap_imagery/src/octomap2Image.cpp
@@ -0,0 +1,49 @@
+//
+#include <iostream>
+#include <string>
+
+// ROS
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+// TF
+#include <tf/transform_listener.h>
+
+// PCL
+#include <pcl_ros/transforms.h>
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/median_filter.h>
+
+// Iterator stuff
+#include <iterator>
+
+using namespace std;
+using namespace pcl;
+using namespace ros;
+
+
+
+
+int main (int argc, char** argv)
+{
+  // Import cloud from .PCD file
+  
+
+  // Load into octomap
+
+  // Visualize with octovis
+
+  // Load into *color* octomap
+
+  // Visualize *in color*
+
+  // Project onto image plane
+
+  // Display image...somehow
+  
+  
+}
