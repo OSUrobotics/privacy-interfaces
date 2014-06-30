e030491333f6d26784623f06414352bd55c628a7
Penn Biggs <yttrium.lead@gmail.com>
2014-06-30 10:15:53 -0700

    Adds map_from_ceiling package

---
+++ map_from_ceiling/src/ceiling_mapper.h
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+#include <iostream>
+// PCL specific includes
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <geometry_msgs/PointStamped.h>
+// For linear algebra. Compile with -O1 -larmadillo
+#include <armadillo>
