0fed470cf41f6c837e45f211e9a0ae0d61eba09d
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-07-02 11:22:43 -0700

    Outline of cloud downsampler node using PCL VoxelGrid filter.

---
+++ octomap_imagery/src/downsample_clouds.cpp
@@ -0,0 +1,25 @@
+// Uses VoxelGrid filter to downsample PointCloud2 messages.
+
+
+// global res
+
+
+// callback
+
+// do filter
+
+// display number of points in vs. out
+
+// publish
+
+
+
+// main
+
+// set res from arg
+
+// subscriber to PointCloud2 /cloud_in
+// publisher to PointCloud2 /cloud_out
+
+// spin
+
