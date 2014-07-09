b22e7a9baccdf7db2d5781750c2c354df841b78f
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-07-09 15:36:30 -0700

    Can now replace foreground points from stored PCD files.

--- octomap_imagery/src/replace_foreground.cpp
+++ octomap_imagery/src/replace_foreground.cpp
@@ -2,60 +2,60 @@
 // Adapted from this tutorial: http://pointclouds.org/documentation/tutorials/octree_change.php
 // Matthew Rueben, Oregon State University
 
+// ROS stuff
+#include <ros/ros.h>
+#include <ros/package.h>
+
+// PCL stuff
 #include <pcl/point_cloud.h>
 #include <pcl/octree/octree.h>
 
+// for PCD file handling in particular
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+// Other stuff
 #include <iostream>
 #include <vector>
-#include <ctime>
+#include <stdlib.h>
 
 int main (int argc, char** argv)
 {
-  srand ((unsigned int) time (NULL));
 
-  // Octree resolution - side length of octree voxels
-  float resolution = 32.0f;
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_back (new pcl::PointCloud<pcl::PointXYZRGB> );
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_fore (new pcl::PointCloud<pcl::PointXYZRGB> );
 
-  // Instantiate octree-based point cloud change detection class
-  pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ> octree (resolution);
+  std::string path = ros::package::getPath("octomap_imagery") + "/data/pcd/replace_foreground/";
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudA (new pcl::PointCloud<pcl::PointXYZ> );
-
-  // Generate pointcloud data for cloudA
-  cloudA->width = 128;
-  cloudA->height = 1;
-  cloudA->points.resize (cloudA->width * cloudA->height);
-
-  for (size_t i = 0; i < cloudA->points.size (); ++i)
+  if ((pcl::io::loadPCDFile<pcl::PointXYZRGB> (path + "test_background.pcd", *cloud_back) == -1) ||
+      (pcl::io::loadPCDFile<pcl::PointXYZRGB> (path + "test_foreground.pcd", *cloud_fore) == -1)) //* load the files
     {
-      cloudA->points[i].x = 64.0f * rand () / (RAND_MAX + 1.0f);
-      cloudA->points[i].y = 64.0f * rand () / (RAND_MAX + 1.0f);
-      cloudA->points[i].z = 64.0f * rand () / (RAND_MAX + 1.0f);
+      PCL_ERROR ("Couldn't read file -- are you in the right directory? \n");
+      return (-1);
+    }
+  
+  // Octree resolution - side length of octree voxels
+  float resolution;
+  if (argc == 2)
+    resolution = atof(argv[1]);
+  else
+    {
+      std::cerr << "Provide the octree resolution as an argument!" << std::endl;
+      return -1;
     }
 
-  // Add points from cloudA to octree
-  octree.setInputCloud (cloudA);
+  // Instantiate octree-based point cloud change detection class
+  pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZRGB> octree (resolution);
+
+  // Add points from cloud_back to octree
+  octree.setInputCloud (cloud_back);
   octree.addPointsFromInputCloud ();
 
   // Switch octree buffers: This resets octree but keeps previous tree structure in memory.
   octree.switchBuffers ();
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudB (new pcl::PointCloud<pcl::PointXYZ> );
-   
-  // Generate pointcloud data for cloudB 
-  cloudB->width = 128;
-  cloudB->height = 1;
-  cloudB->points.resize (cloudB->width * cloudB->height);
-
-  for (size_t i = 0; i < cloudB->points.size (); ++i)
-    {
-      cloudB->points[i].x = 64.0f * rand () / (RAND_MAX + 1.0f);
-      cloudB->points[i].y = 64.0f * rand () / (RAND_MAX + 1.0f);
-      cloudB->points[i].z = 64.0f * rand () / (RAND_MAX + 1.0f);
-    }
-
-  // Add points from cloudB to octree
-  octree.setInputCloud (cloudB);
+  // Add points from cloud_fore to octree
+  octree.setInputCloud (cloud_fore);
   octree.addPointsFromInputCloud ();
 
   std::vector<int> newPointIdxVector;
@@ -63,12 +63,21 @@ int main (int argc, char** argv)
   // Get vector of point indices from octree voxels which did not exist in previous buffer
   octree.getPointIndicesFromNewVoxels (newPointIdxVector);
 
-  // Output points
-  std::cout << "Output from getPointIndicesFromNewVoxels:" << std::endl;
+  std::cout << "Replacing this many points:" << newPointIdxVector.size() << std::endl;
+
+  // Replace foreground points with background points
   for (size_t i = 0; i < newPointIdxVector.size (); ++i)
-    std::cout << i << "# Index:" << newPointIdxVector[i]
-              << "  Point:" << cloudB->points[newPointIdxVector[i]].x << " "
-              << cloudB->points[newPointIdxVector[i]].y << " "
-              << cloudB->points[newPointIdxVector[i]].z << std::endl;
+    {
+      cloud_fore->points[newPointIdxVector[i]].r = cloud_back->points[newPointIdxVector[i]].r;
+      cloud_fore->points[newPointIdxVector[i]].g = cloud_back->points[newPointIdxVector[i]].g;
+      cloud_fore->points[newPointIdxVector[i]].b = cloud_back->points[newPointIdxVector[i]].b;
+      cloud_fore->points[newPointIdxVector[i]].x = cloud_back->points[newPointIdxVector[i]].x;
+      cloud_fore->points[newPointIdxVector[i]].y = cloud_back->points[newPointIdxVector[i]].y;
+      cloud_fore->points[newPointIdxVector[i]].z = cloud_back->points[newPointIdxVector[i]].z;
+    }
+
+  std::cout << "Replaced those points!" << std::endl;
+
+  pcl::io::savePCDFileASCII (path + "test_foreground_replaced.pcd", *cloud_fore);
 
 }
