d08d932c264345eb602a6c342858e0a82c33578c
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-07-09 17:33:44 -0700

    Foreground replacement works for stationary camera!

--- octomap_imagery/src/replace_foreground.cpp
+++ octomap_imagery/src/replace_foreground.cpp
@@ -1,17 +1,20 @@
 // Replaces foreground stuff with background stuff, essentially preventing an image from changing.
 // Adapted from this tutorial: http://pointclouds.org/documentation/tutorials/octree_change.php
+// ...as well as this one: http://pointclouds.org/documentation/tutorials/openni_grabber.php
 // Matthew Rueben, Oregon State University
 
 // ROS stuff
 #include <ros/ros.h>
 #include <ros/package.h>
 #include <sensor_msgs/Image.h>
+#include <pcl_ros/point_cloud.h>  // allows subscribing/publishing PCL types as ROS msgs
 
 // PCL stuff
 #include <pcl/point_cloud.h>
 #include <pcl/octree/octree.h>
 #include <pcl/conversions.h>
 #include <pcl/visualization/image_viewer.h>
+#include <pcl/io/openni_grabber.h>
 
 // for PCD file handling in particular
 #include <pcl/io/pcd_io.h>
@@ -22,31 +25,20 @@
 #include <vector>
 #include <stdlib.h>
 
-int main (int argc, char** argv)
+
+
+// Globals :-(
+float resolution = 0.02f;
+pcl::visualization::ImageViewer viewer ("View Point Clouds as Images!");
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_back;
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_fore;
+bool is_first_cloud = true;
+
+
+void replace_foreground (float resolution,
+			 pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_back, 
+			 pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_fore)
 {
-
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_back (new pcl::PointCloud<pcl::PointXYZRGB> );
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_fore (new pcl::PointCloud<pcl::PointXYZRGB> );
-
-  std::string path = ros::package::getPath("octomap_imagery") + "/data/pcd/replace_foreground/";
-
-  if ((pcl::io::loadPCDFile<pcl::PointXYZRGB> (path + "test_background.pcd", *cloud_back) == -1) ||
-      (pcl::io::loadPCDFile<pcl::PointXYZRGB> (path + "test_foreground.pcd", *cloud_fore) == -1)) //* load the files
-    {
-      PCL_ERROR ("Couldn't read file -- are you in the right directory? \n");
-      return (-1);
-    }
-  
-  // Octree resolution - side length of octree voxels
-  float resolution;
-  if (argc == 2)
-    resolution = atof(argv[1]);
-  else
-    {
-      std::cerr << "Provide the octree resolution as an argument!" << std::endl;
-      return -1;
-    }
-
   // Instantiate octree-based point cloud change detection class
   pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZRGB> octree (resolution);
 
@@ -66,9 +58,8 @@ int main (int argc, char** argv)
   // Get vector of point indices from octree voxels which did not exist in previous buffer
   octree.getPointIndicesFromNewVoxels (newPointIdxVector);
 
-  std::cout << "Replacing this many points:" << newPointIdxVector.size() << std::endl;
-
   // Replace foreground points with background points
+  std::cout << "Replacing this many points:" << newPointIdxVector.size() << std::endl;
   for (size_t i = 0; i < newPointIdxVector.size (); ++i)
     {
       cloud_fore->points[newPointIdxVector[i]].r = cloud_back->points[newPointIdxVector[i]].r;
@@ -80,13 +71,42 @@ int main (int argc, char** argv)
     }
 
   std::cout << "Replaced those points!" << std::endl;
+}
 
 
-  // Display filtered image
-  //sensor_msgs::Image image;
-  //pcl_ros::toROSMsg(*cloud_fore, image);
-  pcl::visualization::ImageViewer viewer ("Filtered Image");
-  viewer.showRGBImage(*cloud_fore);
-  viewer.spin();
+void cloud_callback (pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_fore)
+{
+  if (is_first_cloud)
+    {
+      std::cout << "Storing first cloud...";
+      cloud_back = cloud_fore->makeShared();
+      is_first_cloud = false;
+      std::cout << "done!" << std::endl;
+    }
 
+  replace_foreground(resolution, cloud_back, cloud_fore);
+
+  if (!viewer.wasStopped())
+    viewer.showRGBImage(*cloud_fore);
+}
+
+
+int main (int argc, char** argv)
+{
+  ros::init(argc, argv, "replace_foreground");
+  ros::NodeHandle node;
+
+  // Octree resolution - side length of octree voxels
+  if (argc == 2)
+    resolution = atof(argv[1]);
+  else
+    {
+      std::cerr << "Provide the octree resolution as an argument!" << std::endl;
+      return -1;
+    }
+  
+  ros::Subscriber sub = node.subscribe("cloud_in", 5, cloud_callback);
+
+  ros::spin();
+  
 }
