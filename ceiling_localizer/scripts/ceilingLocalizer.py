8e33ca79a84b90310196c72b85f5f6fdf3412c71
Levi Scott <lscott15@cornellcollege.edu>
2014-07-24 11:29:45 -0500

    Created new package for ceiling_localizer

--- ceiling_localizer/scripts/ceilingLocalizer.py
+++ ceiling_localizer/scripts/ceilingLocalizer.py
@@ -8,17 +8,22 @@ import tf
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import *
 from image_geometry import PinholeCameraModel
+from collections import deque
 
 
-from rosCV import rosCV as rcv
+from cv_bridge import CvBridge
 
-# The ranges for different colors
-#RED = (((0,50,50),(10,255,255))((160,50,50),(179,255,255)))
-#BLUE = ((90,50,50),(120,255,255))
+# This script is used to localize the robot relative to an overhead camera and then
+# publish a frame to tf that is the parent of base_footprint.  There must be a 
+# transformation from the /map frame to the optical frame of the overhead camera for
+# this method to be used for actual localization.  At present the front of the robot
+# is marked by the largest red section in the image and the rear of the robot is
+# marked by the largest blue section in the image.  From this the midpoint is 
+# calculated for position and the vector from blue -> red is used for orientation.
 
 
 class ceilingLocalizer():
-	def __init__(self, image_topic, camera_topic, robot_depth):
+	def __init__(self, image_topic, camera_topic, robot_depth, robot_frame):
 
 		# A subscriber to the image topic
 		rospy.Subscriber(image_topic, Image, self.image_callback)
@@ -32,11 +37,16 @@ class ceilingLocalizer():
 		# How far is the robot plane from the camera?
 		self.robot_depth = robot_depth
 
-		# Alex's image stuff?
-		self.rcv = rcv()
+		# What is the name of the frame we should publish?
+		self.robot_frame = robot_frame
+
+		self.bridge = CvBridge()
 
 		# Publish frames to tf
 		self.broadcaster = tf.TransformBroadcaster()
+
+		# Publisher for edited video
+		self.video = rospy.Publisher('ceilingMarker', Image)
 
 	def camera_callback(self, cameraInfo):
 		self.cameraInfo = cameraInfo
@@ -44,7 +54,8 @@ class ceilingLocalizer():
 		self.cameraFrame = cameraInfo.header.frame_id
 
 	def image_callback(self, image_in):
-		image_cv2 = self.rcv.toCv2(image_in)
+		image_cv = self.bridge.imgmsg_to_cv(image_in, 'bgr8')
+		image_cv2 = numpy.array(image_cv, dtype=numpy.uint8)
 		#image_cv2 = cv2.GaussianBlur(image_cv2,(5,5), 100, 100)
 
 		# Convert the image from BGR to HSV
@@ -86,38 +97,44 @@ class ceilingLocalizer():
 		# Find the midpoint
 		midx = (frontx+backx)/2
 		midy = (fronty+backy)/2
-
-		# Find the unit vector
-		distance = math.sqrt(math.pow(backx-frontx, 2) + math.pow(backy-fronty, 2))
-		uvx = (frontx-backx) / distance
-		uvy = (fronty-backy) / distance
-
-		cv2.line(image_cv2, (frontx, fronty), (backx, backy), (0,255,0), 5)
+		
 		#cv2.circle(image_cv2,(midx,midy), 10, (0,255,0),cv2.cv.CV_FILLED,8,0)
 
-		# The adjacent side of our triangle (from kinect to floor minus turtlebot height)
-		adjacent = self.robot_depth
-
 		[vx,vy,vz] = self.camModel.projectPixelTo3dRay((midx,midy))
-		marker_depth = adjacent / vz
+		marker_depth = self.robot_depth / vz
 		
 		# Position of the robot relative to ceiling kinect
 		x = marker_depth * vx
 		y = marker_depth * vy
-		z = adjacent
+		z = self.robot_depth
 
 		# Orientation of robot
 		yaw = math.atan2((fronty-backy), (frontx-backx))
 		quaternion = tf.transformations.quaternion_from_euler(0.0,0.0,yaw, axes='sxyz')
 
 		# Broadcast the frame to tf
-		self.broadcaster.sendTransform((x,y,z), quaternion, rospy.Time.now(), "base_top", self.cameraFrame)
+		self.broadcaster.sendTransform((x,y,z), quaternion, rospy.Time.now(), self.robot_frame, self.cameraFrame)
+
+		# Draw a line for debug
+		self.draw_orientation(image_cv2, (frontx, fronty), (backx, backy))
 
 		# Show all the images
 		cv2.imshow('mask_red', mask_red_out)
 		cv2.imshow('mask_blue', mask_blue_out)
 		cv2.imshow('image', image_cv2)
 		cv2.waitKey(3)
+
+		# Convert back to ROS Image msg
+		image_cv = cv.fromarray(image_cv2)
+		image_out = self.bridge.cv_to_imgmsg(image_cv, 'bgr8') 
+		self.video.publish(image_out)
+
+	def draw_orientation(self, image, front, back):
+		# Draw a line across the whole image for debug
+		slope = float(front[1] - back[1]) / float(front[0] - back[0])
+		leftmosty = int((-front[0]*slope) + front[1])
+		rightmosty = int(((image.shape[1]-front[0])*slope)+front[1])
+		cv2.line(image, (image.shape[1]-1, rightmosty), (0, leftmosty), (0,255,0), 5)
 
 	def find_marker(self, mask):
 		contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -136,7 +153,8 @@ if __name__ == '__main__':
 	image_topic = rospy.get_param('ceilingLocalizer/image_topic', "/ceiling/rgb/image_color")
 	camera_topic = rospy.get_param('ceilingLocalizer/camera_topic', "/ceiling/rgb/camera_info")
 	robot_depth = rospy.get_param('ceilingLocalizer/robot_depth', 2.289175)
+	robot_frame = rospy.get_param('ceilingLocalizer/robot_frame', "base_top")
 
-	cf = ceilingLocalizer(image_topic, camera_topic, robot_depth)
+	cf = ceilingLocalizer(image_topic, camera_topic, robot_depth, robot_frame)
 
 	rospy.spin()
