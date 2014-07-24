9f00423e861bd1e154b68919d158df8c8996753d
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-07-24 13:26:17 -0700

    Some text filters to try.

--- text_filters/scripts/bit_shifter_gray.py
+++ text_filters/scripts/bit_shifter_gray.py
@@ -17,6 +17,8 @@ def image_callback(image):
     image_cv = bridge.imgmsg_to_cv(image)
     image_cv2 = numpy.asarray(image_cv)
     image_cv2 = cv2.blur(image_cv2, (5, 5))
+    gray = image_cv2[:, :, 0]/3 + image_cv2[:, :, 1]/3 + image_cv2[:, :, 2]/3
+    image_cv2[:, :, 0] = image_cv2[:, :, 1] = image_cv2[:, :, 2] = gray
     image_cv2 = image_cv2 // shift_by * shift_by
     image.data = bridge.cv_to_imgmsg(cv.fromarray(image_cv2), 
                                      encoding=image.encoding).data
@@ -24,7 +26,7 @@ def image_callback(image):
 
 
 if __name__ == "__main__":
-    rospy.init_node("bit_shifter")
+    rospy.init_node("bit_shifter_gray")
     shift_by = int(sys.argv[1])
     rospy.loginfo('Shifting by a factor of {0}'.format(shift_by))
     rospy.Subscriber("/camera/rgb/image_color", Image, image_callback)
