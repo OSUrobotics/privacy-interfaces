8a5a981b68487884ce152b192bdb1d0a74c0c700
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-08-18 14:49:32 -0700

    Probabilistic filter now bags up results.

--- probability_filters/scripts/filter_face_probably.py
+++ probability_filters/scripts/filter_face_probably.py
@@ -41,38 +41,61 @@ class FaceTracker():
     """ Keeps track of face location. """
     def __init__(self):
         self.classifier = cv2.CascadeClassifier(os.environ['ROS_ROOT'] + '/../OpenCV/haarcascades/haarcascade_frontalface_alt.xml')
-        self.tracker = Kalman(Q=.002, R=0.01, P=.01)  # default values
+        self.tracker = Kalman(Q=.002, R=0.01, P=.01) 
 
     def detect_face(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = self.classifier.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in faces:
-            r = max(h, w)/2
             self.tracker.update([x + w/2, 
                                  y + h/2, 
-                                 r])
-            cv2.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
+                                 w, 
+                                 h])
+            cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 1)
             cv2.circle(image, (x + w/2, y + h/2), 3, (0,255,0), -1)
 
-        [u, v, r] = self.tracker.values()  # get estimates
+        [u, v, w, h] = self.tracker.values()  # get estimates
         p = sum(self.tracker.p) / len(self.tracker.p)
 
         cv2.circle(image, (int(u), int(v)), 3, (0,0,255), -1)  # location estimate
-        cv2.circle(image, (int(u), int(v)), int(r), (0,0,255), 2)  # location uncertainty
+        cv2.rectangle(image, (int(u - w/2), int(v - h/2)), (int(u + w/2), int(v + h/2)), (0,0,255), 1)  # location uncertainty
         cv2.imshow('Image with Face(s)', image)
         cv2.waitKey(10)
 
-        return [u, v, r]
+        return [u, v, w, h]
         
         
 class ProbabilityFilter(ImageHandler, FaceTracker):
     """ Filters objects in images based on uncertain location data. """
-    def __init__(self):
+    def __init__(self, bag_path):
         ImageHandler.__init__(self)
         FaceTracker.__init__(self)
+        self.bag_out = rosbag.Bag(bag_path, 'w')
+        self.topic_image = 'image'
+        self.topic_mask = 'mask'
 
     def run_detect_face(self):
-        [u, v, r] = self.detect_face(self.image)
+        self.face = []
+        [u, v, w, h] = self.detect_face(self.image)
+        self.face.append(u - w/2)
+        self.face.append(v - h/2)
+        self.face.append(u + w/2)
+        self.face.append(v + h/2)
+        self.mask = self.make_mask(self.image)
+
+    def make_mask(self, image):
+        mask = numpy.zeros(image.shape)
+        mask[self.face[1] : self.face[3],
+                  self.face[0] : self.face[2], :] = 255
+        mask = mask.astype('uint8')
+        return mask
+
+    def write_face(self):
+        image_msg = self.bridge.cv2_to_imgmsg(self.image, "bgr8")
+        mask_msg = self.bridge.cv2_to_imgmsg(self.mask, "bgr8")
+        self.bag_out.write(self.topic_image, image_msg)
+        self.bag_out.write(self.topic_mask, mask_msg)
+        rospy.loginfo('Wrote a frame to bag file!')
 
 
 def filter_face():
@@ -81,30 +104,29 @@ def filter_face():
     
     # Handle arguments
     if len(sys.argv) < 2:
-        rospy.logerr('Must pass at least the first argument: the bag file path.')
+        rospy.logerr('Must pass at least the first argument: the INPUT bag file path.')
+        return -1
+    if len(sys.argv) < 3:
+        rospy.logerr('Must pass at least the second argument: the OUTPUT bag file path.')
         return -1
     else:
-        bag_path = sys.argv[1] 
-    
+        path_bag_in = sys.argv[1] 
+        path_bag_out = sys.argv[2] 
+
+    # Initialize object(s)
     #face_filter = ProbabilityFilter([640.0/2, 480.0/2])
     #face_filter = ProbabilityFilter([320, 100])
-    face_filter = ProbabilityFilter()
-    face_filter.open_bag(bag_path)
+    face_filter = ProbabilityFilter(path_bag_out)
+    face_filter.open_bag(path_bag_in)
 
     while True:
         try:
             face_filter.get_image()
+            face_filter.run_detect_face()
+            face_filter.write_face()
         except StopIteration:
             rospy.loginfo('Processed last image; exiting!')
             return 0
-    
-        face_filter.run_detect_face()
-
-    # Update Kalman filter
-
-    # Apply filter
-
-    # Display image
 
 
 if __name__ == "__main__":
