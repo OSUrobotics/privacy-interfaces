d5bcee7106f2086faa5aaed307d41de3aa861e2f
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-09-10 16:35:32 -0700

    Parsing script for AMCL poses...and simulation to match!

--- probability_filters/scripts/parse_amcl_for_filtering.py
+++ probability_filters/scripts/parse_amcl_for_filtering.py
@@ -1,14 +1,142 @@
 #!/usr/bin/env python
 
 import rospy
+from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, PoseArray
+import numpy
+import tf
+from tf.transformations import *
 
 
-# Inputs: use_particles = False, sigmas = 2
+class AmclParser():
 
-# Subscribe to /amcl_pose for PoseWithCovariance and to /particlecloud for PoseArray
+    def __init__(self, use_particles=False, confidence_in_sigmas=2):
 
-# 0) Shift from the /base_link or whatever to /camera_rgb_optical_frame
-# 1) Get min & max values for each degree of freedom (e.g. x, y, yaw)
-# 2) Permute into, here, 8 triples
+        self.conf = confidence_in_sigmas  # confidence level
 
-# Publish /camera_pose_extremes as, e.g., Float32MultiArray
+        self.lis = tf.TransformListener()
+
+        self.pub = rospy.Publisher('/camera_pose_extremes', PoseArray)
+
+        if use_particles:
+            rospy.loginfo('Subscribing to cloud of AMCL poses...')
+            rospy.logwarn('Ignoring any specified confidence levels!')
+            rospy.Subscriber('/particlecloud', PoseArray, self.particle_callback)
+        else:
+            rospy.loginfo('Subscribing to AMCL-estimated pose with covariance.')
+            rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, self.covariance_callback)
+
+
+    def particle_callback(self, poses):
+        
+        # Aggregate pose data by degree of freedom (x, y, rotation about z)
+        x = []; y = []; w = []
+        for pose in poses.poses:
+            q = [pose.orientation.x,
+                 pose.orientation.y,
+                 pose.orientation.z,
+                 pose.orientation.w]
+            x_rotation, y_rotation, z_rotation = euler_from_quaternion(q)
+            x.append(pose.position.x)
+            y.append(pose.position.y)
+            w.append(z_rotation)
+        
+        # Find min & max of each degree of freedom
+        x_min = min(x); x_max = max(x)
+        y_min = min(y); y_max = max(y)
+        w_min = min(w); w_max = max(w)
+        
+        # Form PoseArray in camera frame; publish!
+        camera_poses = permute_pose_ranges_2d([[x_min, x_max], 
+                                               [y_min, y_max], 
+                                               [w_min, w_max]])
+        camera_poses.header.frame_id = poses.header.frame_id
+        camera_poses.header.stamp = rospy.Time.now()
+        self.pub.publish(camera_poses)
+
+
+    def covariance_callback(self, pose):
+
+        # Grab variances and convert to standard deviations
+        cov = numpy.asarray(pose.pose.covariance)
+        cov.resize((6, 6))
+        var = numpy.diag(cov)
+        sdev = numpy.sqrt(var)
+
+        # Enforce confidence level
+        x_min = pose.pose.pose.position.x - self.conf * sdev[0]
+        x_max = pose.pose.pose.position.x + self.conf * sdev[0]
+        y_min = pose.pose.pose.position.y - self.conf * sdev[1]
+        y_max = pose.pose.pose.position.y + self.conf * sdev[1]
+        q = [pose.pose.pose.orientation.x,
+             pose.pose.pose.orientation.y,
+             pose.pose.pose.orientation.z,
+             pose.pose.pose.orientation.w]
+        x_rotation, y_rotation, z_rotation = euler_from_quaternion(q)
+        w_min = z_rotation - self.conf * sdev[5]
+        w_max = z_rotation + self.conf * sdev[5]
+
+        # Form PoseArray in camera frame; publish!
+        camera_poses = self.permute_pose_ranges_2d([[x_min, x_max], 
+                                               [y_min, y_max], 
+                                               [w_min, w_max]])
+        camera_poses.header.frame_id = pose.header.frame_id
+        camera_poses = self.transform_pose_array(camera_poses, 
+                                                 '/camera_rgb_optical_frame',
+                                                 '/base_link')
+        camera_poses.header.stamp = rospy.Time.now()
+        self.pub.publish(camera_poses)
+
+
+    def permute_pose_ranges_2d(self, ranges):
+        """ INPUT ranges = [[x_min, x_max], [y_min, y_max], [w_min, w_max]] """
+        poses = PoseArray()
+        for x in ranges[0]:
+            for y in ranges[1]:
+                for w in ranges[2]:
+                    pose = Pose()
+                    [pose.position.x,
+                     pose.position.y,
+                     pose.position.z] = [x, y, 0.0]
+                    [pose.orientation.x,
+                     pose.orientation.y,
+                     pose.orientation.z,
+                     pose.orientation.w] = quaternion_from_euler(0.0, 0.0, w)
+                    poses.poses.append(pose)
+        return poses
+            
+    def transform_pose_array(self, pose_array, source_frame, target_frame):
+
+        # Look up transform from [base] to [camera]
+        translation, rotation = self.lis.lookupTransform(target_frame, source_frame, rospy.Time(0))
+
+        for pose in pose_array.poses:
+
+            # Translate
+            pose.position.x += translation[0]
+            pose.position.y += translation[1]
+            pose.position.z += translation[2]
+
+            # Rotate
+            [pose.orientation.x,
+             pose.orientation.y,
+             pose.orientation.z,
+             pose.orientation.w] = quaternion_multiply([pose.orientation.x,
+                                                        pose.orientation.y,
+                                                        pose.orientation.z,
+                                                        pose.orientation.w], rotation)
+
+        return pose_array
+        
+
+if __name__ == "__main__":
+
+    rospy.init_node('amcl_parser')
+
+    # Get params
+    use_particles = rospy.get_param('~use_particles', False)
+    confidence_in_sigmas = rospy.get_param('~confidence_in_sigmas', 2)
+
+    # Run parser
+    parser = AmclParser(use_particles, confidence_in_sigmas)
+    rospy.spin()
+    
--- probability_filters/scripts/simulate_amcl_and_camera.py
+++ probability_filters/scripts/simulate_amcl_and_camera.py
@@ -4,7 +4,7 @@ import rospy
 import cv2
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image, CameraInfo
-from geometry_msgs.msg import Pose, PoseArray, Point32, PolygonStamped
+from geometry_msgs.msg import Pose, PoseArray, Point32, PolygonStamped, PoseWithCovarianceStamped
 import random
 import tf
 from tf.transformations import *
@@ -33,6 +33,7 @@ class SimulatedRobot():
         self.pub_image = rospy.Publisher('/camera/rgb/image_color', Image)
         self.pub_camera_info = rospy.Publisher('/camera/rgb/camera_info', CameraInfo)
         self.pub_amcl = rospy.Publisher('/particlecloud', PoseArray)
+        self.pub_amcl_pose = rospy.Publisher('/amcl_pose', PoseWithCovarianceStamped)
         self.br = tf.TransformBroadcaster()
         self.pub_polygon = rospy.Publisher('/object_bounds', PolygonStamped)
 
@@ -59,6 +60,7 @@ class SimulatedRobot():
         self.pub_image.publish(self.image_msg)
         self.pub_camera_info.publish(self.camera_info)
         self.pub_amcl.publish(self.amcl)
+        self.pub_amcl_pose.publish(self.amcl_pose)
         self.br.sendTransform((4.0, 3.0, 0.6),
                               (0.0, 0.0, 0.0, 1.0),
                               rospy.Time.now(),
@@ -69,6 +71,11 @@ class SimulatedRobot():
                                         quaternion_about_axis(-pi/2, (0,0,1)))),
                               rospy.Time.now(),
                               '/camera_rgb_optical_frame',
+                              '/map')
+        self.br.sendTransform((2.0, 3.0, 0.0),
+                              (0.0, 0.0, 0.0, 1.0),
+                              rospy.Time.now(),
+                              '/base_link',
                               '/map')
         self.pub_polygon.publish(self.polygon)
 
@@ -90,8 +97,22 @@ class SimulatedRobot():
             pose.orientation.y = q[1]
             pose.orientation.z = q[2]
             pose.orientation.w = q[3]
-            
+
             self.amcl.poses.append(pose)
+
+        self.amcl_pose = PoseWithCovarianceStamped()
+        self.amcl_pose.header.frame_id = "/map"
+        self.amcl_pose.header.stamp = rospy.Time.now()
+        [self.amcl_pose.pose.pose.position.x,
+         self.amcl_pose.pose.pose.position.y,
+         self.amcl_pose.pose.pose.position.z] = [2.0, 3.0, 0.0]
+        [self.amcl_pose.pose.pose.orientation.x,
+         self.amcl_pose.pose.pose.orientation.y,
+         self.amcl_pose.pose.pose.orientation.z,
+         self.amcl_pose.pose.pose.orientation.w] = [0.0, 0.0, 0.0, 1.0]
+        self.amcl_pose.pose.covariance[0]  = dev**2  # set variances = sdev^2
+        self.amcl_pose.pose.covariance[7]  = dev**2
+        self.amcl_pose.pose.covariance[35] = dev**2
 
     def update_polygon(self, s):
         self.polygon = PolygonStamped()  # erase old polygon
