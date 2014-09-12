a28ad78a43e20240f9a62edf9ab8b32f34cae477
MatthewRueben <ruebenm@onid.oregonstate.edu>
2014-09-12 14:27:19 -0700

    Filter launches and runs, but needs debugging.

--- probability_filters/scripts/bounding_box_object_filter.py
+++ probability_filters/scripts/bounding_box_object_filter.py
@@ -1,14 +1,63 @@
 #!/usr/bin/env python
 
 import rospy
+import tf
+from sensor_msgs.msg import CameraInfo, Image, PointCloud
+from image_geometry import PinholeCameraModel
+from cv_bridge import CvBridge
+import cv2
 
-# Listen to /private_object tf frame
-# Subscribe to /object_bounds (PolygonStamped)
-# Subscribe to /camera_pose_extremes (perhaps a Float32MultiArray)
 
-# 3) Convert <x, y, a> to <r, theta, yaw>
-# 4) Choose four corner poses (each containing one of r_min, r_max, yaw_min, yaw_max)
-# 5) Project our PolygonStamped onto those four poses
-# 6) (Run convex hull and then) plot the rectangle (polygon)
+class BoundingBoxFilter():
+    def __init__(self):
 
-# Publish /image_out (Image)
+        self.need_info = True
+        self.model = PinholeCameraModel()
+
+        rospy.Subscriber('/camera/rgb/camera_info', CameraInfo, self.info_callback)
+
+        while self.need_info:
+            rospy.sleep(0.01)
+
+        rospy.loginfo('Got CameraInfo! Proceeding to filter.')
+
+        self.have_projections = False
+        rospy.Subscriber('/extreme_vertex_projections', PointCloud, self.projections_callback)
+        self.bridge = CvBridge()
+        rospy.Subscriber('/camera/rgb/image_color', Image, self.image_callback)
+        
+        self.image_pub = rospy.Publisher('/image_out', Image)
+
+    def info_callback(self, info):
+        if self.need_info:
+            self.model.fromCameraInfo(info)
+            self.need_info = False
+
+    def projections_callback(self, projections):
+        self.projections = projections
+        if not self.have_projections:
+            self.have_projections = True
+
+    def image_callback(self, image):
+        if self.have_projections:
+            array = self.bridge.imgmsg_to_cv2(image, "bgr8")
+            #cv2.fillConvexPoly(array,
+            #numpy.asarray(polygon),
+            #(0, 0, 255))
+            for point in self.projections.points:
+                u, v = self.model.project3dToPixel((point.x,
+                                                  point.y,
+                                                  point.z))
+                print u, v
+                cv2.circle(array, (int(u), int(v)), 3, (255, 0, 0))
+            print
+            image_new = self.bridge.cv2_to_imgmsg(array, "bgr8")
+            image_new.header.stamp = rospy.Time.now()
+            self.image_pub.publish(image_new)
+    
+
+if __name__ == '__main__':
+
+    rospy.init_node('bounding_filter')
+    boundingBoxFilter = BoundingBoxFilter()
+    rospy.spin()
--- probability_filters/scripts/find_extreme_viewpoints.py
+++ probability_filters/scripts/find_extreme_viewpoints.py
@@ -3,22 +3,37 @@
 import rospy
 from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import PointCloud
-from geometry_msgs.msg import Point32
+from geometry_msgs.msg import Point32, PolygonStamped
 import numpy
 import tf
 from tf.transformations import *
-from math import atan2, sin, cos, pi
+from math import atan2, sin, cos, pi, sqrt
 
 
 class ExtremeViewpointFinder():
 
     def __init__(self):
         
+        self.need_bounds = True
+        
         self.lis = tf.TransformListener()
 
-        self.pub = rospy.Publisher('/camera_viewpoint_extremes', PointCloud)
+        self.pub = rospy.Publisher('/extreme_vertex_projections', PointCloud)
+
+        rospy.Subscriber('/object_bounds_map', PolygonStamped, self.bounds_callback)
+        
+        while self.need_bounds:  # block until we get object boundaries
+            rospy.sleep(0.01)
+
+        rospy.loginfo('Got object bounds! Proceeding to find extreme projections.')
 
         rospy.Subscriber('/robot_pose_ranges', Float32MultiArray, self.ranges_callback)
+        
+
+    def bounds_callback(self, bounds):
+        if self.need_bounds:
+            self.bounds = bounds
+            self.need_bounds = False
 
 
     def ranges_callback(self, ranges):
@@ -69,8 +84,9 @@ class ExtremeViewpointFinder():
         theta = theta[facing_object]
         yaw = yaw[facing_object]
 
-        # Find min/max of radius & yaw
+        # Find min/max of radius, theta, & yaw
         r_min = min(r); r_max = max(r)
+        theta_min = min(theta); theta_max = max(theta)
         yaw_min = min(yaw); yaw_max = max(yaw)
 
         ###### Switch to the POV of the Camera ######
@@ -79,17 +95,35 @@ class ExtremeViewpointFinder():
         base_views.header.frame_id = '/base_footprint'
         base_views.header.stamp = rospy.Time(0)
         for r_i in [r_min, r_max]:
-            for yaw_i in [yaw_min, yaw_max]:
-                base_view = Point32()
-                base_view.x = r_i * cos(-yaw_i)
-                base_view.y = r_i * sin(-yaw_i)
-                base_view.z = obj[2] - 0.0  # since base_footprint is at z = 0
-                base_views.points.append(base_view)
-        camera_views = self.lis.transformPointCloud('/camera_rgb_optical_frame', base_views)
+            for theta_i in [theta_min, theta_max]:
+                for yaw_i in [yaw_min, yaw_max]:
+                    for point in self.bounds.polygon.points:  # resolve object bounds here because it'd be costly to do it 125,000 times
+
+                        base = [r_i * cos(theta_i),  # viewpoint relative to object *center*, /map frame
+                                r_i * sin(theta_i)]
+
+                        base_rel = [b - p for b, p in zip(base, [point.x,  # viewpoint relative to object *vertex*, /map frame
+                                                                 point.y])]
+
+                        # Calculate new radius and yaw (relative to object vertex instead of center)
+                        r_new = sqrt(sum([el**2 for el in base_rel]))
+                        cosine = sum([b + br for b, br in zip(base, base_rel)])  # calculate dot product by hand
+                        sine = base[0] * base_rel[1] - base[1] * base_rel[0]  # calculate cross product by hand
+                        d_yaw = -1 * atan2(sine, cosine)
+                        yaw_new = yaw_i + d_yaw
+
+                        if yaw_new > -pi/2 and yaw_new < pi/2:  # abort if we're not facing the object vertex
+
+                            # Object *vertex* location from perspective of /base_footprint
+                            base_view = Point32()
+                            base_view.x = r_new * cos(-yaw_new)
+                            base_view.y = r_new * sin(-yaw_new)
+                            base_view.z = obj[2] - 0.0  # since base_footprint is at z = 0
+                            base_views.points.append(base_view)
+
+        camera_views = self.lis.transformPointCloud('/camera_rgb_optical_frame', base_views)  # transform to camera frame
 
         self.pub.publish(camera_views)
-
-
         
 
 if __name__ == "__main__":
